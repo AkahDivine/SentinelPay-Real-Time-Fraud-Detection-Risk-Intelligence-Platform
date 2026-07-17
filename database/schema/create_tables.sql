25f21f7e1ed843f373cd5e836a46ed9d285dd8e6
Akah Divine <119632710+AkahDivine@users.noreply.github.com>
2026-07-17 21:24:37 +0100

    Update create_tables.sql

--- database/schema/create_tables.sql
+++ database/schema/create_tables.sql
@@ -82,8 +82,24 @@ CREATE TABLE dim_customers (
         REFERENCES dim_location (location_key)
 );
 
+-- 5. customer_devices
+CREATE TABLE customer_devices (
+	customer_device_key		INTEGER NOT NULL,
+	customer_key 			INTEGER NOT NULL,
+	device_key				INTEGER NOT NULL,
+	is_primary				BOOLEAN,
+	status					VARCHAR(15),
+	first_seen				TIMESTAMP,
+	last_seen				TIMESTAMP,
 
--- 5. dim_merchants
+	CONSTRAINT pk_customer_devices PRIMARY KEY (customer_device_key),
+	CONSTRAINT fk_customers_key FOREIGN KEY (customer_key)
+			REFERENCES dim_customers (customer_key),
+	CONSTRAINT fk_devices_key FOREIGN KEY (device_key)
+			REFERENCES dim_device (device_key)
+);
+
+-- 6. dim_merchants
 CREATE TABLE dim_merchants (
     merchant_key          INTEGER      NOT NULL,
     merchant_id           VARCHAR(20)  NOT NULL,
@@ -98,11 +114,11 @@ CREATE TABLE dim_merchants (
 );
 
 
--- 6. dim_accounts
+-- 7. dim_accounts
 CREATE TABLE dim_accounts (
     account_key           INTEGER      NOT NULL,
     customer_key          INTEGER      NOT NULL,
-    account_id            VARCHAR(20),
+    account_id            VARCHAR(20)  NOT NULL,
     account_type          VARCHAR(20),
     balance               NUMERIC(18,2),
     daily_limit           NUMERIC(18,2),
@@ -116,7 +132,7 @@ CREATE TABLE dim_accounts (
 );
 
 
--- 7. fact_transactions
+-- 8. fact_transactions
 CREATE TABLE fact_transactions (
     transaction_key           INTEGER      NOT NULL,
     customer_key              INTEGER      NOT NULL,
@@ -125,14 +141,14 @@ CREATE TABLE fact_transactions (
     device_key                INTEGER      NOT NULL,
     location_key              INTEGER      NOT NULL,
     date_key                  INTEGER      NOT NULL,
-    transaction_id            VARCHAR(20),
-    amount                    NUMERIC(18,2),
-    transaction_type          VARCHAR(25),
-    transaction_datetime      TIMESTAMP,
-    channel                   VARCHAR(50),
-    transaction_status        VARCHAR(20),
-	failure_reason            VARCHAR(50),
-    transaction_direction     VARCHAR(20),
+    transaction_id            VARCHAR(20)  NOT NULL,
+    amount                    NUMERIC(18,2)NOT NULL,
+    transaction_type          VARCHAR(25)  NOT NULL,
+    transaction_datetime      TIMESTAMP    NOT NULL,
+    channel                   VARCHAR(50)  NOT NULL,
+    transaction_status        VARCHAR(20)  NOT NULL,
+	failure_reason            VARCHAR(100),
+    transaction_direction     VARCHAR(20)  NOT NULL,
     counterparty_account_id   VARCHAR(20),
     counterparty_type         VARCHAR(20),
     counterparty_risk_score   INTEGER,
@@ -153,7 +169,7 @@ CREATE TABLE fact_transactions (
 );
 
 
--- 8. fact_risk_scores
+-- 9. fact_risk_scores
 CREATE TABLE fact_risk_scores (
     score_id 			  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     transaction_key       INTEGER      NOT NULL,
@@ -170,14 +186,13 @@ CREATE TABLE fact_risk_scores (
     merchant_flag         BOOLEAN,
     inflow_flag           BOOLEAN,
 
-    CONSTRAINT pk_fact_risk_scores PRIMARY KEY (score_id),
     CONSTRAINT fk_risk_scores_transaction FOREIGN KEY (transaction_key)
         REFERENCES fact_transactions (transaction_key),
     CONSTRAINT fk_risk_scores_customer FOREIGN KEY (customer_key)
         REFERENCES dim_customers (customer_key)
 );
 
--- 9. fraud_alerts
+-- 10. fraud_alerts
 CREATE TABLE fraud_alerts (
     alert_id			  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     transaction_key       INTEGER      NOT NULL,
@@ -189,9 +204,8 @@ CREATE TABLE fraud_alerts (
     alert_status          VARCHAR(20),
     created_at            TIMESTAMP,
     resolved_at           TIMESTAMP,
-	resolution_comment    VARCHAR(225)
+	resolution_comment    VARCHAR(225),
 
-    CONSTRAINT pk_fraud_alerts PRIMARY KEY (alert_id),
     CONSTRAINT fk_alerts_transaction FOREIGN KEY (transaction_key)
         REFERENCES fact_transactions (transaction_key),
     CONSTRAINT fk_alerts_customer FOREIGN KEY (customer_key)
@@ -199,7 +213,7 @@ CREATE TABLE fraud_alerts (
 );
 
 
--- 10. fact_fraud_events
+-- 11. fact_fraud_events
 CREATE TABLE fact_fraud_events (
     fraud_id 			  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     transaction_key       INTEGER      NOT NULL,
@@ -213,7 +227,6 @@ CREATE TABLE fact_fraud_events (
     detected_at           TIMESTAMP,
     resolved_at           TIMESTAMP,
 
-    CONSTRAINT pk_fact_fraud_events PRIMARY KEY (fraud_id),
     CONSTRAINT fk_fraud_events_transaction FOREIGN KEY (transaction_key)
         REFERENCES fact_transactions (transaction_key),
     CONSTRAINT fk_fraud_events_customer FOREIGN KEY (customer_key)
@@ -223,47 +236,45 @@ CREATE TABLE fact_fraud_events (
 );
 
 
-
+-- 12. account_balance_history
 CREATE TABLE account_balance_history (
     history_key       INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    account_key       INTEGER      NOT NULL,
-    customer_key      INTEGER      NOT NULL,
+    account_key       INTEGER       NOT NULL,
+    customer_key      INTEGER       NOT NULL,
     transaction_key   INTEGER,
-    movement_type     VARCHAR(10)  NOT NULL,  -- DEBIT or CREDIT
-    amount            NUMERIC(18,2),
-    balance_before    NUMERIC(18,2),
-    balance_after     NUMERIC(18,2),
-    recorded_at       TIMESTAMP,
+    movement_type     VARCHAR(10)   NOT NULL,  
+    amount            NUMERIC(18,2) NOT NULL,
+    balance_before    NUMERIC(18,2) NOT NULL,
+    balance_after     NUMERIC(18,2) NOT NULL,
+    recorded_at       TIMESTAMP     NOT NULL,
  
-    CONSTRAINT fk_abh_account
-        FOREIGN KEY (account_key)
+    CONSTRAINT fk_abh_account FOREIGN KEY (account_key)
         REFERENCES dim_accounts(account_key),
-    CONSTRAINT fk_abh_customer
-        FOREIGN KEY (customer_key)
+    CONSTRAINT fk_abh_customer FOREIGN KEY (customer_key)
         REFERENCES dim_customers(customer_key)
 );
 
 
-
+-- 13. rejected_transactions
 CREATE TABLE rejected_transactions (
     rejection_key        INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    customer_key          INTEGER,
-    account_key           INTEGER,
+    customer_key          INTEGER		  NOT NULL,
+    account_key           INTEGER		  NOT NULL,
     merchant_key          INTEGER,
-    amount                NUMERIC(18,2),
-    transaction_type      VARCHAR(25),
-    transaction_datetime  TIMESTAMP,
-    channel               VARCHAR(50),
-    transaction_direction VARCHAR(20),
-    rejection_reason      VARCHAR(100),
-    attempted_at          TIMESTAMP,
+    amount                NUMERIC(18,2)   NOT NULL,
+    transaction_type      VARCHAR(25)     NOT NULL,
+    transaction_datetime  TIMESTAMP       NOT NULL,
+    channel               VARCHAR(50)     NOT NULL,
+    transaction_direction VARCHAR(20)     NOT NULL,
+    rejection_reason      VARCHAR(100)	  NOT NULL,
+    attempted_at          TIMESTAMP       NOT NULL,
  
-    CONSTRAINT fk_rt_customer
-        FOREIGN KEY (customer_key)
+    CONSTRAINT fk_rt_customer FOREIGN KEY (customer_key)
         REFERENCES dim_customers(customer_key),
-    CONSTRAINT fk_rt_account
-        FOREIGN KEY (account_key)
-        REFERENCES dim_accounts(account_key)
+    CONSTRAINT fk_rt_account FOREIGN KEY (account_key)
+        REFERENCES dim_accounts(account_key),
+    CONSTRAINT fk_rt_merchant FOREIGN KEY (merchant_key)
+    	REFERENCES dim_merchants(merchant_key)
 );
 
 
